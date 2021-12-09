06eae07f7109c72f68080c333d4276c95ba105be
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2021-12-09 06:46:32 -0800

    #19 chore: code cleanup and maintenance (#20)

--- src/App.js
+++ src/App.js
@@ -11,23 +11,23 @@ import TeamSection from "./components/sections/TeamSection";
 import Details from "./components/sections/Details";
 
 function App() {
-  const featuresRef = useRef(null);
-  const teamRef = useRef(null);
-  const contactRef = useRef(null);
-  const bannerRef = useRef(null)
+	const featuresRef = useRef(null);
+	const teamRef = useRef(null);
+	const contactRef = useRef(null);
+	const bannerRef = useRef(null)
 
-  return (
-    <div className="App">
-      <Header featuresRef={ featuresRef } teamRef={ teamRef } contactRef={ contactRef } bannerRef={ bannerRef } />
-      <Banner1 featuresRef={ featuresRef } bannerRef={ bannerRef } />
-      <FeaturesSection featuresRef={ featuresRef } />
-      <Details />
-      <FactsSection />
-      <TeamSection teamRef={ teamRef } />
-      <ContactSection contactRef={ contactRef } />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header featuresRef={featuresRef} teamRef={teamRef} contactRef={contactRef} bannerRef={bannerRef} />
+			<Banner1 featuresRef={featuresRef} bannerRef={bannerRef} />
+			<FeaturesSection featuresRef={featuresRef} />
+			<Details />
+			<FactsSection />
+			<TeamSection teamRef={teamRef} />
+			<ContactSection contactRef={contactRef} />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
